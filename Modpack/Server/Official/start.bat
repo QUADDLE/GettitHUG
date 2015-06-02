5898e7e9f15f4907ecf7aa2f5a07f0dfbaf7ffe0
G1nux <g1nux@web.de>
2015-06-02 15:58:54 +0200

    Started Project on Github
    Let's go.

---
+++ Modpack/Server/Official/start.bat
@@ -0,0 +1,1 @@
+java -Xmx1024M -Xms1024M -jar forge-1.8-11.14.1.1419-universal.jar nogui
---
+++ Modpack/Server/Test/launch.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\Program Files\Java\jre7\bin\java.exe" -Xmx3G -Xms2G -jar minecraft_server.1.7.10.jar nogui
