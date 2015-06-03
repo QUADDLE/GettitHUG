b71a622ecf3e271c3f7bffc00fed571e58e41d10
G1nux <g1nux@web.de>
2015-06-03 23:40:04 +0200

    Sorry...
    Den Server werde ich leider nicht mehr veröffentlichen - vielleicht gebe
    ich die Mods frei und biete eine Lite-Version als Download an - aber so
    konfiguriert und mit eigenen Mods (wie ich ihn für die offiziellen
    Gettit-Server verwende) gebe ich ihn nicht raus - ausserdem noch aus
    folgenden Gründen:
    
    - diverse Plugin-Configs enthalten einige MySQL-Daten
    - die Welt soll die Welt von Gettit bleiben - um vor Kopierern zu
    schützen

--- Modpack/Server/Official/start.bat
+++
@@ -1,1 +0,0 @@
-java -Xmx1024M -Xms1024M -jar forge-1.8-11.14.1.1419-universal.jar nogui
--- Modpack/Server/Test/launch.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-"C:\Program Files\Java\jre7\bin\java.exe" -Xmx3G -Xms2G -jar minecraft_server.1.7.10.jar nogui
